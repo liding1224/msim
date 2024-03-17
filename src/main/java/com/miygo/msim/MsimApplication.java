c8328e4876aae7405fefd25c27d874e7e06f6aea
liding <newlife1224@126.com>
2024-03-17 18:00:10 +0800

    搭建SpringBoot

---
+++ src/main/java/com/miygo/msim/MsimApplication.java
@@ -0,0 +1,11 @@
+package com.miygo.msim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsimApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsimApplication.class, args);
+	}
+}
---
+++ src/main/java/com/miygo/msim/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.miygo.msim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("SysName", "订单管理系统");
+        return "modules/home/index";
+    }
+}
